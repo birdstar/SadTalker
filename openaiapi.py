f8dd90d63897556fe48c5b95f9036693095568da
Wang <pengwa1@capel0079213841.cn133.corpintra.net>
2024-03-31 13:52:23 +0800

    add log

--- openaiapi.py
+++ openaiapi.py
@@ -7,8 +7,11 @@ app = FastAPI()
 @app.get("/process/{name}")
 async def process_audio(name: str):
     command = f"python inference.py --driven_audio /tmp/{name}.wav --source_image demo/wangpeng.png --enhancer gfpgan --output {name}"
+    print(command)
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process.communicate()
+    print("output:",output)
+    print("error:",error)
 
     if process.returncode == 0:
         return PlainTextResponse(content="Process completed successfully")
